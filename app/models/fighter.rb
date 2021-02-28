7db1693e183c028d3622f7a4ae9306f08e9c6e05
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-02-28 11:52:22 +0900

    responsive

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,6 +1,6 @@
 class Fighter < ApplicationRecord
   belongs_to :warband
-
+  ROLES = ["leader", "soldier", "levy"]
   validates :value, presence: true
   validates :name, presence: true
   validates :role, presence: true
--- app/models/warband.rb
+++ app/models/warband.rb
@@ -1,6 +1,8 @@
 class Warband < ApplicationRecord
   has_many:fighters, dependent: :destroy
-
+  has_one_attached :photo
+  FACTIONS =["the living", "the dead"]
   validates :name, presence: true, uniqueness: true
-
+  validates :faction, presence:true
+  validates :role, presence:true
 end
